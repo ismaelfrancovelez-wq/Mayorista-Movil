0addf30420dabd8a84c1412850c303ebe79a4539
Ismael Franco <ismaelfrancovelez@gmail.com>
2026-03-23 21:12:15 -0300

    1233213212432143aaaaa0a1121231111112

--- app/dashboard/pedidos-fraccionados/layout.tsx
+++ app/dashboard/pedidos-fraccionados/layout.tsx
@@ -1,16 +1,7 @@
-import RevendedorSidebar from "../../../components/RevendedorSidebar";
-
 export default function RevendedorLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <div className="flex min-h-screen bg-gray-100">
-      <RevendedorSidebar />
-      <main className="flex-1 p-8">
-        {children}
-      </main>
-    </div>
-  );
+  return <>{children}</>;
 }
