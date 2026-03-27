7e7e732f5363ddf53b64edd47822bf6abc03de8a
Ismael Franco <ismaelfrancovelez@gmail.com>
2026-03-27 20:02:32 -0300

    1233213212432143aaaaa0a1121231111112112211+11741

--- components/HomePrincipal.tsx
+++ components/HomePrincipal.tsx
@@ -663,17 +663,17 @@ export default function HomePrincipal({
               <p className="text-gray-600">En compras de cantidades menores al minimo, podras hacer la reserva de tu producto sin necesidad de pagar. Una ves hecha tu reserva, te unes a la barra de progreso junto a otros compradores para llegar al minimo del producto establecido. </p>
             </div>
             <div className="text-center">
-              <div className="w-20 h-20 bg-gradient-to-br from-blue-500 to-blue-600 rounded-2xl flex items-center justify-center text-4xl mx-auto mb-4 transform hover:scale-110 transition-all shadow-xl"><span className="font-black text-white drop-shadow-md tracking-tight">2</span></div>
-              <h4 className="text-xl font-bold text-gray-900 mb-2">Reserva tu producto</h4>
-              <p className="text-gray-600">En compras de cantidades menores al minimo, podras hacer la reserva de tu producto sin necesidad de pagar. Una ves hecha tu reserva, te unes a la barra de progreso junto a otros compradores para llegar al minimo del producto establecido. </p>
+              <div className="w-20 h-20 bg-gradient-to-br from-blue-500 to-blue-600 rounded-2xl flex items-center justify-center text-4xl mx-auto mb-4 transform hover:scale-110 transition-all shadow-xl"><span className="font-black text-white drop-shadow-md tracking-tight">3</span></div>
+              <h4 className="text-xl font-bold text-gray-900 mb-2">Baja de reserva</h4>
+              <p className="text-gray-600">Mientras el lote no se halla completado podras darte de baja sin ninguna tipo de penalizacion. Una ves completado el lote deberas realizar el pago sin exepcion, ya que la reserva constituye un compromiso comunitario entre todos los compradores. </p>
             </div>
             <div className="text-center">
-              <div className="w-20 h-20 bg-gradient-to-br from-blue-500 to-blue-600 rounded-2xl flex items-center justify-center text-4xl mx-auto mb-4 transform hover:scale-110 transition-all shadow-xl"><span className="font-black text-white drop-shadow-md tracking-tight">3</span></div>
+              <div className="w-20 h-20 bg-gradient-to-br from-blue-500 to-blue-600 rounded-2xl flex items-center justify-center text-4xl mx-auto mb-4 transform hover:scale-110 transition-all shadow-xl"><span className="font-black text-white drop-shadow-md tracking-tight">4</span></div>
               <h4 className="text-xl font-bold text-gray-900 mb-2">Pago</h4>
               <p className="text-gray-600">Una ves que el lote se completa, se te notificara dentro de la plataforma y por medio de WhatsApp, junto con un link de pago por correo electronico.</p>
             </div>
             <div className="text-center">
-              <div className="w-20 h-20 bg-gradient-to-br from-blue-500 to-blue-600 rounded-2xl flex items-center justify-center text-4xl mx-auto mb-4 transform hover:scale-110 transition-all shadow-xl"><span className="font-black text-white drop-shadow-md tracking-tight">4</span></div>
+              <div className="w-20 h-20 bg-gradient-to-br from-blue-500 to-blue-600 rounded-2xl flex items-center justify-center text-4xl mx-auto mb-4 transform hover:scale-110 transition-all shadow-xl"><span className="font-black text-white drop-shadow-md tracking-tight">5</span></div>
               <h4 className="text-xl font-bold text-gray-900 mb-2">Recibí tu pedido</h4>
               <p className="text-gray-600">Una ves que todos los compradores hallan pagado, se libera la totalidad del dinero hacia el vendedor, y tu resives el producto entre 24 a 72hs.</p>
             </div>
