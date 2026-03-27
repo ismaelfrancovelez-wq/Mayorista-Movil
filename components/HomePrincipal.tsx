fd0224e3b9012fc5ee9ba780838c3ba34fd9a250
Ismael Franco <ismaelfrancovelez@gmail.com>
2026-03-27 19:58:02 -0300

    1233213212432143aaaaa0a1121231111112112211+117

--- components/HomePrincipal.tsx
+++ components/HomePrincipal.tsx
@@ -655,12 +655,17 @@ export default function HomePrincipal({
             <div className="text-center">
               <div className="w-20 h-20 bg-gradient-to-br from-blue-500 to-blue-600 rounded-2xl flex items-center justify-center text-4xl mx-auto mb-4 transform hover:scale-110 transition-all shadow-xl"><span className="font-black text-white drop-shadow-md tracking-tight">1</span></div>
               <h4 className="text-xl font-bold text-gray-900 mb-2">Elegí tu producto</h4>
-              <p className="text-gray-600">Busca en el explorador, el producto que mas quieras o necesites.</p>
+              <p className="text-gray-600">Busca en el explorador, el producto que quieras o necesites, y elije entre compra directa o fraccionada.</p>
             </div>
             <div className="text-center">
               <div className="w-20 h-20 bg-gradient-to-br from-blue-500 to-blue-600 rounded-2xl flex items-center justify-center text-4xl mx-auto mb-4 transform hover:scale-110 transition-all shadow-xl"><span className="font-black text-white drop-shadow-md tracking-tight">2</span></div>
               <h4 className="text-xl font-bold text-gray-900 mb-2">Reserva tu producto</h4>
-              <p className="text-gray-600">En compras de cantidades menores al minimo, podras hacer la reserva de tu producto sin necesidad de pagar. Una ves hecha tu reserva, te unes a la barra de progreso junto a otros compradores para llegar al minimo del producto establecido. Mientras el lote no se halla completado podras darte de baja sin ninguna tipo de penalizacion. </p>
+              <p className="text-gray-600">En compras de cantidades menores al minimo, podras hacer la reserva de tu producto sin necesidad de pagar. Una ves hecha tu reserva, te unes a la barra de progreso junto a otros compradores para llegar al minimo del producto establecido. </p>
+            </div>
+            <div className="text-center">
+              <div className="w-20 h-20 bg-gradient-to-br from-blue-500 to-blue-600 rounded-2xl flex items-center justify-center text-4xl mx-auto mb-4 transform hover:scale-110 transition-all shadow-xl"><span className="font-black text-white drop-shadow-md tracking-tight">2</span></div>
+              <h4 className="text-xl font-bold text-gray-900 mb-2">Reserva tu producto</h4>
+              <p className="text-gray-600">En compras de cantidades menores al minimo, podras hacer la reserva de tu producto sin necesidad de pagar. Una ves hecha tu reserva, te unes a la barra de progreso junto a otros compradores para llegar al minimo del producto establecido. </p>
             </div>
             <div className="text-center">
               <div className="w-20 h-20 bg-gradient-to-br from-blue-500 to-blue-600 rounded-2xl flex items-center justify-center text-4xl mx-auto mb-4 transform hover:scale-110 transition-all shadow-xl"><span className="font-black text-white drop-shadow-md tracking-tight">3</span></div>
